52034666fa69564dd562c4e740fd3ae5dc504705
Ellis G <egaraudy@gmail.com>
2020-07-09 14:06:44 -0600

    balloon shooter

---
+++ Assets/Scripts/Popper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popper : MonoBehaviour
+{
+    public GameObject arCamera;
+    public GameObject smoke;
+
+    public void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit))
+        {
+            if (hit.transform.tag == "Balloon")
+            {
+                Destroy(hit.transform.gameObject);
+            }
+
+            Instantiate(smoke, hit.point, Quaternion.LookRotation(hit.normal));
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnBalloons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBalloons : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+
+    public GameObject[] balloons;
+
+    public int liveBalloons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+    }
+
+    IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(4);
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            Instantiate(balloons[i], spawnPoints[i].position, Quaternion.identity);
+            liveBalloons++;
+        }
+        
+        StartCoroutine(StartSpawning());
+    }
+    
+}
